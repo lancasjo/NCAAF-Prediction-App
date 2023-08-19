dd94a9d65d5825d35ede975545f96ff6d8675e27
lancasjo <104534650+lancasjo@users.noreply.github.com>
2023-08-19 14:34:21 -0400

    Added security rules

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
 const app = express();
 const port = 3000; // Choose a port number
 
 const uri = "mongodb+srv://jrlancaste:bugbugbug@sportsbetting.vqijjoh.mongodb.net/?retryWrites=true&w=majority";
+
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
 const client = new MongoClient(uri);
 
 app.get('/api/data', async (req, res) => {
     try {
-
+      
       await client.connect();
   
       const database = client.db('game-database'); // Replace with your database name
